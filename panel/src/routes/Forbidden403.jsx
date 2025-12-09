ce30992855b8d609b9c442e18951953de71c8d7f
fsdFEWGRAE <17sutef3@gmail.com>
2025-12-09 04:23:29 +0300

    Create Forbidden403.jsx

---
+++ panel/src/routes/Forbidden403.jsx
@@ -0,0 +1,10 @@
+import "./403.css";
+
+export default function Forbidden403() {
+  return (
+    <div className="bloody403-container">
+      <h1>403 — You Do Not Have Permission</h1>
+      <p>You are not authorized to access this page.</p>
+    </div>
+  );
+}
