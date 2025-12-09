761e07a06eb1420e9234bc08f6700bda000d5c22
fsdFEWGRAE <17sutef3@gmail.com>
2025-12-09 04:21:24 +0300

    Update AuthContext.jsx

--- panel/src/context/AuthContext.jsx
+++ panel/src/context/AuthContext.jsx
@@ -1,18 +1,29 @@
 import { createContext, useState, useEffect } from "react";
+
 export const AuthContext = createContext();
+
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-  useEffect(() =
+
+  useEffect(() => {
     const savedUser = localStorage.getItem("user");
     if (savedUser) setUser(JSON.parse(savedUser));
   }, []);
-  const login = (data) =
+
+  const login = (data) => {
     localStorage.setItem("token", data.token);
     localStorage.setItem("user", JSON.stringify(data.user));
     setUser(data.user);
   };
-  const logout = () =
+
+  const logout = () => {
     localStorage.clear();
     setUser(null);
   };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
