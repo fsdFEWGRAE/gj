643fe66990f43e08c761e85615d281d2a8be32a0
fsdFEWGRAE <17sutef3@gmail.com>
2025-12-09 04:42:13 +0300

    Update axiosInstance.js

--- panel/src/axiosInstance.js
+++ panel/src/axiosInstance.js
@@ -1,8 +1,16 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "/glom/api" });
-api.interceptors.request.use((config) =
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+
+const api = axios.create({
+  baseURL: "/glom/api",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
